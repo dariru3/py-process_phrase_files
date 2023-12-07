dbebee13aa63cafe27e59f3f4b5d1fd577fc64d4
Daryl V <daryl.villalobos@hotmail.com>
2023-12-07 10:37:05 +0900

    Date format changes

--- excel.py
+++ excel.py
@@ -8,7 +8,7 @@ def process_excel(file_path):
     sheet = wb.sheets[0]
 
     # Determine the last row and column
-    last_row = sheet.range('A' + str(sheet.cells.last_cell.row)).end('up').row
+    last_row = sheet.range('C' + str(sheet.cells.last_cell.row)).end('up').row
     last_column = sheet.range('XFD1').end('left').column
 
     # Create a new workbook
@@ -18,7 +18,7 @@ def process_excel(file_path):
     # Read and copy the data, skipping the first two rows and columns
     for row in range(3, last_row + 1):
         values = sheet.range((row, 3), (row, last_column)).value
-        new_sheet.range((row - 2, 1)).value = values
+        new_sheet.range((row - 2, 1), (row - 2, last_column - 2)).value = values
 
     # Save the processed workbook
     processed_file_path = file_path.replace('.xlsx', '_processed.xlsx')
