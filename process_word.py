34ee554fdee4b834a0106ab73600c90ee5c9afc0
Daryl V <daryl.villalobos@hotmail.com>
2023-12-12 17:21:07 +0900

    update process word with color function

--- process_word.py
+++ process_word.py
@@ -1,5 +1,14 @@
 from docx import Document
 import os
+from docx.oxml.ns import nsdecls
+from docx.oxml import parse_xml
+
+def change_table_cell(cells, background_color=None):
+    """Changes the background color of a table cell."""
+    for cell in cells:
+        if background_color:
+            shading_elm = parse_xml(r'<w:shd {} w:fill="{}"/>'.format(nsdecls('w'), background_color))
+            cell._tc.get_or_add_tcPr().append(shading_elm)
 
 def process_word_file(file_path):
     doc = Document(file_path)
@@ -13,19 +22,23 @@ def process_word_file(file_path):
     # Process the fourth table (now the first table in the document)
     if len(doc.tables) > 0:
         original_table = doc.tables[0]
-        rows = original_table.rows
 
         # Create a new table with 4 columns
         new_table = doc.add_table(rows=0, cols=4)
+        # Add column numbers to copy to list
+        columns_to_copy = [2, 3, 5, 6]
 
-        for row in rows:
+        for row in original_table.rows:
             new_row = new_table.add_row()
             new_cells = new_row.cells
             # Copy the content from the original table
-            new_cells[0].text = row.cells[2].text  # 3rd column
-            new_cells[1].text = row.cells[3].text  # 4th column
-            new_cells[2].text = row.cells[5].text  # 5th column
-            new_cells[3].text = row.cells[6].text  # 6th column
+            for i, col_index in enumerate(columns_to_copy):
+                new_cells[i].text = row.cells[col_index].text
+
+            # If the english column cell has text, highlight cells gray
+            if new_cells[2].text.strip():
+                cells_to_color = [new_cells[1], new_cells[2]]
+                change_table_cell(cells_to_color, background_color="D9D9D9")  # Gray color
 
         # Remove the original table
         original_table._element.getparent().remove(original_table._element)
