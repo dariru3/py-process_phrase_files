b4aa398857426d3054feda03f365907fd0851b71
Daryl V <daryl.villalobos@hotmail.com>
2024-02-19 15:44:09 +0900

    add index

--- df_to_word.py
+++ df_to_word.py
@@ -7,17 +7,19 @@ output_file_path = "output_files/df_to_word_output.docx"
 
 def dataframe_to_word_table(df, output_file_path):
     doc = Document()
-    table = doc.add_table(rows=1, cols=len(df.columns))
+    table = doc.add_table(rows=1, cols=len(df.columns) + 1)
     
     # Add header row
+    table.cell(0,0).text = 'Index'
     for i, column in enumerate(df.columns):
-        table.cell(0, i).text = str(column)
+        table.cell(0, i + 1).text = str(column)
     
     # Add data rows
     for index, row in df.iterrows():
         cells = table.add_row().cells
+        cells[0].text = str(index)
         for i, value in enumerate(row):
-            cells[i].text = str(value)
+            cells[i + 1].text = str(value)
     
     doc.save(output_file_path)
     print(f"Word document has been saved to {output_file_path}.")
