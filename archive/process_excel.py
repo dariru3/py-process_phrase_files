baf35588b5f1f5eb34db0aa81f2f8173bf8e2207
Daryl V <daryl.villalobos@hotmail.com>
2024-03-25 15:58:29 +0900

    rename excel file process vars

--- archive/process_excel.py
+++ archive/process_excel.py
@@ -1,7 +1,7 @@
 import xlwings as xw
 import os
 
-def process_excel(file_path, output_folder):
+def pre_process_excel(file_path, output_folder):
     # Start an instance of Excel and open the workbook
     app = xw.App(visible=False)
     wb = app.books.open(file_path)
@@ -16,27 +16,27 @@ def process_excel(file_path, output_folder):
     # Construct new file path
     base_name = os.path.basename(file_path)
     name_part, extension = os.path.splitext(base_name)
-    new_filename = name_part + '_processed' + extension
-    processed_file_path = os.path.join(output_folder, new_filename)
+    new_filename = name_part + '_pre-processed' + extension
+    pre_processed_file_path = os.path.join(output_folder, new_filename)
 
     # Check for output folder
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     # Save the changes to the workbook
-    wb.save(processed_file_path)
+    wb.save(pre_processed_file_path)
 
     # Close the workbook and quit the Excel application
     wb.close()
     app.quit()
 
-    print(f"Processed file: {processed_file_path}")
+    print(f"Pre-processed file: {pre_processed_file_path}")
 
-def process_all_excel_in_folder(folder_path, output_folder):
+def pre_process_all_excel_in_folder(folder_path, output_folder):
     for file_name in os.listdir(folder_path):
         if file_name.endswith('.xlsx'):
             file_path = os.path.join(folder_path, file_name)
-            process_excel(file_path, output_folder)
+            pre_process_excel(file_path, output_folder)
 
 if __name__ == "__main__":
-    process_all_excel_in_folder('input_files', 'output_files')
+    pre_process_all_excel_in_folder('input_files', 'output_files')
